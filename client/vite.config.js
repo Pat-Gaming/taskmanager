db3899aa96565cfe3d288752df28aefb71230261
Pat-Gaming <95402776+Pat-Gaming@users.noreply.github.com>
2024-05-02 16:19:39 +0800

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://localhost:8800",
+        target: "https://cloud-based-manager.onrender.com",
         changeOrigin: true,
       },
     },
